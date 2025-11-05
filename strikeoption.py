09a8979c37bb9a3de1894a21750fb0f356067a6b
ElimelechEhrlich <ee525566@gmail.com>
2025-11-05 20:45:27 +0200

    Add range attribute to StrikeOption and subclasses

--- strikeoption.py
+++ strikeoption.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
 class StrikeOption(ABC):
-    def __init__(self, name:str, ammo:int):
+    def __init__(self, name:str, ammo:int, range:str):
         self.name = name
         self.ammo = ammo
+        self.range = range
     @abstractmethod
     def strike(self):
         pass
@@ -12,7 +13,7 @@ class Tank(StrikeOption):
     def strike(self):
         if self.ammo < 0:
             self.ammo -= 1
-            return f'strike: Shell firing'
+            return f'strike: Shell firing, range: {self.range}'
         else:
             return 'Out of ammo'
     
@@ -20,7 +21,7 @@ class Rifle(StrikeOption):
     def strike(self):
         if self.ammo < 0:
             self.ammo -= 1
-            return f'strike: shooting with rifle'
+            return f'strike: shooting with rifle, range: {self.range}'
         else:
             return 'Out of ammo'
     
@@ -28,7 +29,7 @@ class SniperRifle(StrikeOption):
     def strike(self):
         if self.ammo < 0:
             self.ammo -= 1
-            return f'strike: shooting with sniper rifle'
+            return f'strike: shooting with sniper rifle, range: {self.range}'
         else:
             return 'Out of ammo'
 
